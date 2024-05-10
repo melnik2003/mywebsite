f81dc6f38878729b68ec1f5856c61037c24b45d9
Мельников Максим Антонович <melnik2003@proton.me>
2024-05-10 20:46:57 +0300

    Added script

---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd PhotoIn || exit
+zip image.zip *
+cd ..
+
+current_time=$(date +"%Y-%m-%d %H-%M-%S") 
+log_file='conversion-log.txt'
+
+echo -n "Создание архива исходных файлов $current_time">> $log_file
+echo -n "Начало обработки файлов $current_time">>$log_file 
+
+cd PhotoIn || exit
+
+for f in *.jpeg;do
+  filename="../PhotoOut/new_$f"
+  if convert "$f" -resize 30% "$filename";then
+    echo "файл $f успешно обработан $current_time. Новоe имя файла $filename" >> ../"$log_file";
+  else
+    echo "ошибка конвертации файла $f $current_time"  >> ../"$log_file";
+  fi 
+done
