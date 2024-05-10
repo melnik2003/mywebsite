9cfa426f88c8fac21d3617bd049b83002c046a70
Мельников Максим Антонович <melnik2003@proton.me>
2024-05-10 23:05:55 +0300

    bs

--- i/script.sh
+++ i/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd PhotoIn || exit
-zip image.zip *
+cd PhotoIn || exit 1
+zip PhotoArh.zip *
 cd ..
 
 current_time=$(date +"%Y-%m-%d %H-%M-%S") 
@@ -10,13 +10,19 @@ log_file='conversion-log.txt'
 echo -n "Создание архива исходных файлов $current_time" > $log_file
 echo -n "Начало обработки файлов $current_time" >> $log_file 
 
-cd PhotoIn || exit
+cd PhotoIn || exit 1
 
-for f in *.jpg; do
-  filename="../PhotoOut/new_$f"
-  if convert "$f" -resize 30% "$filename";then
-    echo "файл $f успешно обработан $current_time. Новоe имя файла $filename" >> ../"$log_file";
-  else
-    echo "ошибка конвертации файла $f $current_time"  >> ../"$log_file";
-  fi 
+for f in PhotoOut/*.jpg
+do
+    convert-im6.q16 -debug all "PhotoIn/$f PhotoOut/$f.png" 2 >> log_file
+    mogrify-im6.q16 -debug all -quality 96 "PhotoOut/$f.png" 2 >> log_file
 done
+
+#for f in *.jpg; do
+#    filename="../PhotoOut/new_$f"
+#    if convert "$f" -resize 30% "$filename";then
+#        echo "файл $f успешно обработан $current_time. Новоe имя файла $filename" >> ../"$log_file";
+#    else
+#        echo "ошибка конвертации файла $f $current_time"  >> ../"$log_file";
+#    fi 
+#done
